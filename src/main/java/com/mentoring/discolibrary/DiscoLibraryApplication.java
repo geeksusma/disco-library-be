de39537dde433f00802c06db3725b4beba48c0f8
geeksusma <jesus.maria.villar.vazquez@gmail.com>
2019-03-20 11:35:37 +0100

    mostly basic structure for the application

---
+++ src/main/java/com/mentoring/discolibrary/DiscoLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.mentoring.discolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoLibraryApplication.class, args);
+	}
+
+}
