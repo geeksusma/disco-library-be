b79899b84225426169c74545e8a9da74ac187046
ozzytrain <PunkyFu_2014>
2019-04-12 13:27:18 +0200

    GetAllStylesController implementation

--- src/main/java/com/mentoring/discolibrary/style/get/GetAllStylesController.java
+++ src/main/java/com/mentoring/discolibrary/style/get/GetAllStylesController.java
@@ -1,9 +1,12 @@
 package com.mentoring.discolibrary.style.get;
 
 import com.mentoring.discolibrary.style.GetAllStylesDAO;
+import com.mentoring.discolibrary.style.Style;
 import com.mentoring.discolibrary.style.StyleDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class GetAllStylesController {
@@ -17,7 +20,25 @@ class GetAllStylesController {
     }
 
     ResponseEntity<List<StyleDTO>> getAll() {
+        List<Style> allStyles = getAllStylesDAO.getAllStyles();
+        List<StyleDTO> respuesta = new ArrayList<>();
 
-        throw new UnsupportedOperationException("implement me!");
+     /*   if (!allStyles.isEmpty()) {
+            for (Style style : allStyles) {
+                respuesta.add(styleApiMapper.mapToDto(style));
+            }
+
+            return new ResponseEntity(respuesta,HttpStatus.OK);
+        }
+        else return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }*/
+
+        for (Style style : allStyles) {
+            respuesta.add(styleApiMapper.mapToDto(style));
+        }
+
+        return new ResponseEntity(respuesta, HttpStatus.OK);
     }
+
 }
+
